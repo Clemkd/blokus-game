d60009cc544ddf871f62545d08d5f9c5127af2fe
andrev <andrev@127.0.0.1>
2016-05-17 17:19:26 +0200

    Modification de Tile

--- blokus-game/src/entities/Tile.java
+++ blokus-game/src/entities/Tile.java
@@ -1,11 +1,56 @@
 package entities;
 
 public class Tile {
+	/**
+	 * Constante pour la hauteur de la matrice d'une Tile
+	 */
+	public static final int HEIGHT = 7;
 	
-	private static final int HEIGHT = 7;
-	private static final int WIDTH = 7;
+	/**
+	 * Constante pour la matrice de la matrice d'une Tile
+	 */
+	public static final int WIDTH = 7;
 	
+	/**
+	 * Matrice d'entier permettant de représenter la forme d'une tuile et les possibilités d'adjacence
+	 */
 	private int[][] matrix = new int[HEIGHT][WIDTH] ;
+	
+	/**
+	 * Couleur de la Tile
+	 */
+	CellColor couleur;
+	
+	/**
+	 * 
+	 * @param matrix la matrice représentant la forme de la Tile à instancier
+	 * @param couleur le type de cellule de la Tile (couleur de la cellule)
+	 */
+	public Tile(int[][] matrix, CellColor couleur) {
+		this.matrix = matrix;
+		this.couleur = couleur;
+	}
+
+	public int[][] getMatrix() {
+		return matrix;
+	}
+
+	public void setMatrix(int[][] matrix) {
+		this.matrix = matrix;
+	}
+
+	public CellColor getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(CellColor couleur) {
+		this.couleur = couleur;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
