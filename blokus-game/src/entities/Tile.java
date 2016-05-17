ca37f9a3efabd1fb13ce73816ee424688860b711
andrev <andrev@127.0.0.1>
2016-05-17 17:00:16 +0200

    Modification de l'uml

--- blokus-game/src/entities/Tile.java
+++ blokus-game/src/entities/Tile.java
@@ -1,5 +1,12 @@
 package entities;
 
 public class Tile {
-
+	
+	private static final int HEIGHT = 7;
+	private static final int WIDTH = 7;
+	
+	private int[][] matrix = new int[HEIGHT][WIDTH] ;
+	
+	
+	
 }
