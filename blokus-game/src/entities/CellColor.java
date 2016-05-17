d29283a649d8ca2a6b27bbe87c7574dc673b8c05
andrev <andrev@127.0.0.1>
2016-05-17 17:55:16 +0200

    Implémentation de Tile et de CellColor

--- blokus-game/src/entities/CellColor.java
+++ blokus-game/src/entities/CellColor.java
@@ -1,9 +1,49 @@
 package entities;
 
+import java.awt.Color;
+
 public enum CellColor {
 	
-	blue,
-	red,
-	green,
-	yellow;
+	/**
+	 * Cellule de couleur bleu avec le chemin d'accès vers l'image correspondante
+	 */
+	BLUE(""),
+	
+	/**
+	 * Cellule de couleur rouge avec le chemin d'accès vers l'image correspondante
+	 */
+	RED(""),
+	
+	/**
+	 * Cellule de couleur verte avec le chemin d'accès vers l'image correspondante
+	 */
+	GREEN(""),
+	
+	/**
+	 * Cellule de couleur jaune avec le chemin d'accès vers l'image correspondante
+	 */
+	YELLOW("");
+	
+	/**
+	 * attribut représentant le chemin d'accès vers l'image correspondate au type de celulle
+	 */
+	private String path;
+	
+	/**
+	 * Constructeur de CellColor
+	 * @param path le chemin d'accès vers une image
+	 */
+	private CellColor(String path){
+		this.path = path;
+	}
+	
+	/**
+	 * accesseur de path
+	 * @return le path
+	 */
+	public String getPath(){
+		return this.path;
+	}
+	
+	
 }
--- blokus-game/src/entities/Tile.java
+++ blokus-game/src/entities/Tile.java
@@ -1,5 +1,7 @@
 package entities;
 
+import utilities.Vector2;
+
 public class Tile {
 	/**
 	 * Constante pour la hauteur de la matrice d'une Tile
@@ -30,28 +32,83 @@ public class Tile {
 		this.matrix = matrix;
 		this.couleur = couleur;
 	}
-
+	
+	/**
+	 * Accesseur de matrix
+	 * @return la matrice de représentation de la Tile
+	 */
 	public int[][] getMatrix() {
 		return matrix;
 	}
-
+	
+	/**
+	 * Mutateur de matrix
+	 * @param matrix nouvelle matrice pour la Tile
+	 */
 	public void setMatrix(int[][] matrix) {
 		this.matrix = matrix;
 	}
-
+	
+	/**
+	 * Accessseur de couleur
+	 * @return le type de cellule
+	 */
 	public CellColor getCouleur() {
 		return couleur;
 	}
-
+	
+	/**
+	 * mutateur de couleur
+	 * @param couleur la nouvelle type de cellule de la Tile
+	 */
 	public void setCouleur(CellColor couleur) {
 		this.couleur = couleur;
 	}
 	
+	/**
+	 * Méthode permettant la rotation dans un sens horaire de la matrice de la Tile
+	 */
+	public void rotateClockwise() {
+		int[][] temp = new int[WIDTH][HEIGHT];
+
+		for (int x = 0; x < WIDTH; x++){
+			for (int y = 0; y < HEIGHT; y++){
+				temp[WIDTH - y - 1][x] = this.matrix[x][y];
+			}
+		}
+
+		matrix = temp;
+	}
+	
+	/**
+	 * Méthode permettant la rotation dans un sens anti-horaire de la matrice de la Tile
+	 */
+	public void rotateCounterClockwise()
+	{
+		int[][] temp = new int[WIDTH][HEIGHT];
+	
+		for (int x = 0; x < WIDTH; x++){
+			for (int y = 0; y < HEIGHT; y++){
+				temp[y][WIDTH - x - 1] = matrix[x][y];
+			}
+		}
 	
 	
+		this.setMatrix(temp);
+	}
 	
 	
-	
-	
-	
+	/**
+	 * Méthode permettant d'effecture une symétrie de la Tile
+	 */
+	public void flip()
+	   {
+	      int[][] temp = new int[WIDTH][HEIGHT];
+	      
+	      for (int x = 0; x < WIDTH; x++)
+	         for (int y = 0; y < HEIGHT; y++)
+	            temp[WIDTH - x - 1][y] = matrix[x][y];
+	            
+	      this.setMatrix(temp);
+	   }
 }
