26457b3c24c0df4841a7af7d50af75ae884136e3
bouixl <loic.bouix@iut-valence.fr>
2016-05-18 15:14:20 +0200

    Liaison Fenetre/Souris

--- blokus-game/src/gui/GraphicsPanel.java
+++ blokus-game/src/gui/GraphicsPanel.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -19,97 +17,82 @@ import javax.swing.JComponent;
 
 import utilities.Vector2;
 
-public class GraphicsPanel extends JComponent implements MouseMotionListener, MouseListener, MouseWheelListener, DrawableInterface {
+public class GraphicsPanel extends JComponent implements MouseMotionListener, MouseListener, MouseWheelListener {
 	private static final long	serialVersionUID	= 1L;
-	private Image background;
+	private Image				background;
 
-
-	
 	public GraphicsPanel() {
 		super();
 	}
-	
+
 	@Override
-	public void paintComponent(Graphics g)
-	{
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D batch = (Graphics2D) g;
 		this.clear(batch);
 		try {
 			this.background = ImageIO.read(new File("./resources/images/background.png"));
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			System.out.println("erreur chargement fond");
 			e.printStackTrace();
 		}
-		
+
 		g.drawImage(background, 0, 0, this.getWidth(), this.getHeight(), null);
 		Navigation.getPage().draw(batch);
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void mouseClicked(MouseEvent e) {
+		Mouse.setLastMouseButton(e.getButton());
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
+	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		Mouse.setLastMouseButton(e.getButton());
+		Mouse.setMouseState(Mouse.MOUSE_PRESSED);
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		Mouse.setLastMouseButton(e.getButton());
+		Mouse.setMouseState(Mouse.MOUSE_RELEASED);
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		Mouse.setLastMouseButton(e.getButton());
+		Mouse.setMouseState(Mouse.MOUSE_PRESSED);
+		Mouse.setMousePosition(new Vector2<Integer>(e.getX(), e.getY()));
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		Mouse.setMousePosition(new Vector2<Integer>(e.getX(), e.getY()));
 	}
-	
-	protected void clear(Graphics2D batch)
-	{
+
+	protected void clear(Graphics2D batch) {
 		batch.setPaint(Color.WHITE);
 		batch.fillRect(0, 0, this.getWidth(), this.getHeight());
 	}
 
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
-		// TODO Auto-generated method stub
-		
+		Mouse.setLastScrollClicks(e.getWheelRotation());
 	}
 
-	@Override
 	public void update(float elapsedTime) {
 		Navigation.getPage().update(elapsedTime);
 		this.repaint();
 	}
-
-	@Override
-	public void draw(Graphics2D g) {
-		this.repaint();
-		
-	}
-
 }
