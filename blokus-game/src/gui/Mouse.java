128ebb5d9f9fdaeaa13e71e908031aece8c666f0
bouixl <loic.bouix@iut-valence.fr>
2016-05-18 14:40:21 +0200

    Timer global + debut arrangement Mouse

--- blokus-game/src/gui/Mouse.java
+++ blokus-game/src/gui/Mouse.java
@@ -1,5 +1,7 @@
 package gui;
 
+import javax.xml.crypto.dsig.CanonicalizationMethod;
+
 import utilities.Vector2;
 
 public class Mouse 
@@ -8,7 +10,8 @@ public class Mouse
 	public static final int LEFT = 2;
 	public static final int WHEEL_SCROLL = 3;
 	
-	private static final int mouseState = LEFT;
+	private static int lastScrollClicks = 0;
+	private static int mouseState = LEFT;
 	private static boolean mouseReleased = true;
 	private static Vector2<Integer> mousePosition = new Vector2<Integer>(0, 0);
 
@@ -29,8 +32,38 @@ public class Mouse
 	public static int getState() {
 		return mouseState;
 	}
-
-	public static void privateSetMouseReleased(boolean mouseReleased) {
+	
+	/**
+	 * /!\ Réservé à l'écouteur de clics de souris /!\
+	 * Change l'état de la souris(appuyé/relaché)
+	 * @param mouseReleased Nouvel état
+	 */
+	public static void setMouseReleased(boolean mouseReleased) {
 		Mouse.mouseReleased = mouseReleased;
+	}
+	
+	/**
+	 * /!\ Réservé à l'écouteur de molette de souris /!\
+	 * Change le nombre de clics perçus par la molette
+	 * @param clicks Nouveau nombre de clics
+	 */
+	public static void setLastScrollClicks(int clicks) {
+		
+		lastScrollClicks = clicks;
+	}
+	
+	/**
+	 * Transmet le nombre de clics(=crans de rotation) enregistrés par la molette de la souris lors de son dernier évènement.
+	 * @return Nombre de clics
+	 */
+	public static int getLastScrollClicks() {
+		return lastScrollClicks;
+	}
+	
+	/**
+	 * Remet à 0 le compteur de clics de la molette, à utiliser après avoir traité ceux-ci.
+	 */
+	public static void consumeLastScroll() {
+		lastScrollClicks = 0;
 	}
 }
--- blokus-game/src/gui/Window.java
+++ blokus-game/src/gui/Window.java
@@ -1,19 +1,21 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class Window implements Runnable , ActionListener 
 {
+	private static final int UPDATE_DELAY = 15;
+	
 	private JFrame frame;
 	private GraphicsPanel gameGraphics;
+
+	private Timer timer;
+
+	private long startTime;
 
 	public Window(String name, int width, int height) {
 		this.frame = new JFrame();
@@ -22,18 +24,24 @@ public class Window implements Runnable , ActionListener
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.gameGraphics = new GraphicsPanel();
 		this.frame.getContentPane().add(this.gameGraphics, BorderLayout.CENTER);
-		
+		this.timer = new Timer(UPDATE_DELAY, this);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==this.timer) {
+			float elapsedTime = (System.nanoTime() - this.startTime)/100000f;
+			this.startTime = System.nanoTime();
+			
+			this.gameGraphics.update(elapsedTime);
+		}
 	}
 
 	@Override
 	public void run() {
 		this.frame.setVisible(true);
 		this.frame.setResizable(false);
+		this.startTime = System.nanoTime();
+		this.timer.start();
 	}
-
 }
