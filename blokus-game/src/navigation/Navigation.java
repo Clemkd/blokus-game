0123ba4d60f79f04ed148df174b551c763c98308
ClementDidier <c.didier.contact@gmail.com>
2016-05-18 17:07:16 +0200

    Javadoc Navigation.java

--- blokus-game/src/navigation/Navigation.java
+++ blokus-game/src/navigation/Navigation.java
@@ -2,14 +2,29 @@ package navigation;
 
 public class Navigation {
 	
+	/**
+	 * L'état de jeu
+	 */
 	private static Page page;
+	
+	/**
+	 * L'état de jeu : Page d'accueil
+	 */
 	public static Page homePage = new HomePage();
 	
+	/**
+	 * Change l'état de jeu et navigue vers cet état
+	 * @param p Le nouvel état de jeu
+	 */
 	public static void NavigateTo(Page p)
 	{
 		page = p;
 	}
 	
+	/**
+	 * Obtient l'état courant du jeu
+	 * @return L'état courant du jeu
+	 */
 	public static Page getPage()
 	{
 		return page;
