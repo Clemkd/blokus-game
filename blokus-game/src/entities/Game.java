0863d3469c4bb8e491931ec66c9a287a608c58f7
bouixl <loic.bouix@iut-valence.fr>
2016-05-23 11:12:12 +0200

    Debut PlayerIA + Ajouts dans Game

--- blokus-game/src/entities/Game.java
+++ blokus-game/src/entities/Game.java
@@ -1,39 +1,42 @@
 package entities;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.List;
 
 import utilities.Move;
 import utilities.UndoRedoManager;
 
 public class Game {
-	private static final int EVENT_TURN_ENDED = 5;
+	private static final int			EVENT_TURN_ENDED	= 5;
 	/**
 	 * Plateau de jeu courant
 	 */
-	protected Board board;
+	protected Board						board;
 	/**
 	 * Liste des joueurs de la partie
 	 */
-	protected ArrayList<Player> players;
+	protected ArrayList<Player>			players;
 	/**
 	 * Tour actuel, aussi utilisé pour determiner le joueur courant
 	 */
-	protected int currentTurn;
+	protected int						currentTurn;
 	/**
-	 * Prise en charge de la sauvegarde de l'état de la partie pour les annulations et répétitions de coups
+	 * Prise en charge de la sauvegarde de l'état de la partie pour les annulations et répétitions
+	 * de coups
 	 */
-	protected UndoRedoManager<Board> undoRedoManager;
+	protected UndoRedoManager<Board>	undoRedoManager;
 	/**
 	 * Liste des ActionListeners de cette partie
 	 */
-	protected ArrayList<ActionListener> listeners;
-	
+	protected ArrayList<ActionListener>	listeners;
+
 	public Game() {
 		this.currentTurn = 0;
-		
+
 		this.board = new Board();
-		
+
 		this.players = new ArrayList<Player>();
 		ArrayList<CellColor> colorsP1 = new ArrayList<CellColor>();
 		colorsP1.add(CellColor.BLUE);
@@ -43,64 +46,73 @@ public class Game {
 		colorsP2.add(CellColor.YELLOW);
 		colorsP2.add(CellColor.GREEN);
 		this.players.add(new PlayerHuman("J2", colorsP2));
-		
+
 		this.undoRedoManager = new UndoRedoManager<Board>();
-		
+
 		this.listeners = new ArrayList<ActionListener>();
 	}
-	
+
 	/**
-	 * Renvoie l'état de la partie, la partie est finie quand aucun joueur ne peut jouer un coup supplémentaire.
+	 * Renvoie l'état de la partie, la partie est finie quand aucun joueur ne peut jouer un coup
+	 * supplémentaire.
+	 * 
 	 * @return Etat de la partie, true si elle est terminée.
 	 */
 	public boolean isTerminated() {
 		return false;
 	}
-	
+
 	/**
 	 * Transmet une copie du plateau courant
+	 * 
 	 * @return Copie du plateau courant
 	 */
 	public Board getBoard() {
 		return this.board.copy();
 	}
-	
+
 	/**
 	 * Permet de récuperer le tour courant
+	 * 
 	 * @return Tour courant
 	 */
 	public int getTurn() {
 		return this.currentTurn;
 	}
-	
-	//TODO: Implémenter le undoRedoManager différemment car besoin sauvegarder état des joueurs aussi(pieces restantes, etc)
+
+	// TODO: Implémenter le undoRedoManager différemment car besoin sauvegarder état des joueurs
+	// aussi(pieces restantes, etc)
 	public void undoMove() {
 		this.board = undoRedoManager.undo(this.board);
 		this.currentTurn--;
 	}
+
 	public void redoMove() {
 		this.board = undoRedoManager.redo(this.board);
 		this.currentTurn++;
 	}
+
 	public boolean canUndo() {
 		return this.undoRedoManager.canUndo();
 	}
+
 	public boolean canRedo() {
 		return this.undoRedoManager.canRedo();
 	}
-	
+
 	/**
 	 * Récupère le joueur courant
+	 * 
 	 * @return Joueur courant
 	 */
 	public Player getCurrentPlayer() {
 		CellColor c = getCurrentColor();
-		
-		for(Player p : this.players) {
-			if(p.colors.contains(c))
+
+		for (Player p : this.players) {
+			if (p.colors.contains(c))
 				return p;
 		}
-		
+
 		return null;
 	}
 
@@ -109,7 +121,7 @@ public class Game {
 	 */
 	private CellColor getCurrentColor() {
 		CellColor c;
-		switch(this.currentTurn%4) {
+		switch (this.currentTurn % 4) {
 			case 0:
 				c = CellColor.BLUE;
 				break;
@@ -126,19 +138,19 @@ public class Game {
 				c = CellColor.BLUE;
 				break;
 		}
-		
+
 		return c;
 	}
-	
+
 	/**
 	 * Appelé à chaque itération de jeu
 	 */
 	public void update() {
-		if(!this.isTerminated()) {
+		if (!this.isTerminated()) {
 			Player p = this.getCurrentPlayer();
-			if(!p.isPlaying()) {
+			if (!p.isPlaying()) {
 				Move m = p.getMove();
-				if(m==null) {
+				if (m == null) {
 					p.play(this.getCurrentColor());
 				}
 				else {
@@ -147,16 +159,17 @@ public class Game {
 			}
 		}
 	}
-	
+
 	/**
-	 * Sauvegarde la partie dans un fichier
-	 * TODO: do it
+	 * Sauvegarde la partie dans un fichier TODO: do it
 	 */
-	//public abstract void save();
-	
+	// public abstract void save();
+
 	/**
 	 * Fonction appellée quand un coup valide doit être traité/appliqué par Game
-	 * @param m Coup joué
+	 * 
+	 * @param m
+	 *            Coup joué
 	 */
 	public void doMove(Move m) {
 		this.undoRedoManager.add(this.board.copy());
@@ -171,30 +184,70 @@ public class Game {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Ajoute un listener à la classe de jeu
-	 * @param al Le listener à ajouter
+	 * 
+	 * @param al
+	 *            Le listener à ajouter
 	 */
 	public void addListener(ActionListener al) {
 		this.listeners.add(al);
 	}
-	
+
 	/**
 	 * Supprime un listener de la classe de jeu
-	 * @param al Le listener à supprimer
+	 * 
+	 * @param al
+	 *            Le listener à supprimer
 	 */
 	public void removeListener(ActionListener al) {
 		this.listeners.remove(al);
 	}
-	
+
 	/**
 	 * Lance l'évènement sur tous les listeners de la classe
-	 * @param e Les informtations de l'évènement lancé
+	 * 
+	 * @param e
+	 *            Les informtations de l'évènement lancé
 	 */
 	public void raiseEvent(ActionEvent e) {
-		for(ActionListener al : this.listeners) {
+		for (ActionListener al : this.listeners) {
 			al.actionPerformed(e);
 		}
+	}
+
+	/**
+	 * Renvoi la liste de tout les coups possibles pour le joueur courant en tenant compte du
+	 * plateau et de son inventaire de pièces.
+	 * 
+	 * @return Liste des coups possibles
+	 */
+	public List<Move> possibleMoves() {
+		ArrayList<Move> res = new ArrayList<Move>();
+
+		return res;
+	}
+
+	/**
+	 * Renvoi un objet Game représentant l'objet courant auquel on a appliqué un mouvement qui ne
+	 * doit pas être pris en compte dans l'historique de la partie
+	 * 
+	 * @param m Coup à jouer
+	 * @return Nouvel état de partie
+	 */
+	public Game simulateMove(Move m) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Renvoi le score du joueur fourni en paramètre dans l'état actuel du jeu
+	 * @param player Joueur
+	 * @return Score du joueur
+	 */
+	public int getScore(Player player) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
---
+++ blokus-game/src/entities/PlayerIA.java
@@ -0,0 +1,73 @@
+package entities;
+
+import java.util.List;
+import java.util.Random;
+
+import utilities.Move;
+
+public class PlayerIA extends Player {
+
+	private static final int MAX_DEPTH = 4;
+	protected Random	rand;
+
+	public PlayerIA(String name, List<CellColor> colors) {
+		super(name, colors);
+		this.rand = new Random();
+	}
+
+	@Override
+	public void play(CellColor c) {
+		//TODO: changer prototype fonction vers public void play(Game origin, CellColor c)
+		//this.chosenMove = this.alphaBeta(origin, MAX_DEPTH, true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+	
+	private Move alphaBeta(Game node, int depth, boolean maximizingPlayer, int alpha, int beta) {
+	    if (depth == 0 || node.isTerminated())
+	        return new Move(this.evaluate(node));
+
+		if (maximizingPlayer) {
+			Move bestMove = new Move(Integer.MIN_VALUE);
+			for (Move m : node.possibleMoves()) {
+				Game nextNode = node.simulateMove(m);
+				m.setValue(alphaBeta(nextNode, depth-1, false, alpha, beta).getValue());
+				if(m.getValue()>bestMove.getValue()) {
+					bestMove = m;
+				}
+				alpha = Math.max(alpha, m.getValue());
+				if (beta <= alpha)
+					break;
+			}
+			return bestMove;
+		}
+		else {
+			Move bestMove = new Move(Integer.MAX_VALUE);
+			for (Move m : node.possibleMoves()) {
+				Game nextNode = node.simulateMove(m);
+				m.setValue(alphaBeta(nextNode, depth-1, true, alpha, beta).getValue());
+				if(m.getValue()<bestMove.getValue()) {
+					bestMove = m;
+				}
+				beta = Math.min(beta, m.getValue());
+				if (beta <= alpha)
+					break;
+			}
+			return bestMove;
+		}
+	}
+
+	private int evaluate(Game node) {
+		int res = node.getScore(this); // La valeur de base correspond au score qu'on peut esperer obtenir
+		/* TODO
+		 * Critères suppélmentaires 
+		 * - Passer à travers 
+		 * - Bloquer adversaire 
+		 * - Surface disponible 
+		 * - Mieux = traverser ou bloquer ? => Paramètres IA, pondération
+		 * - Aller vers le centre
+		 * - Fonction d'évaluation de minimax à améliorer
+		 * - Phases de jeu: début, milieu, fin
+		 * - Ameliorer fonction coups possibles, moins de mises à jour, sauvegarde de ce qui existe
+		 */
+		return res;
+	}
+}
