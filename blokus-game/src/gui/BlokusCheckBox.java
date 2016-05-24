bfddfc175febac10b0bbf48c20505c2499d6f240
andrev <andrev@127.0.0.1>
2016-05-24 17:15:11 +0200

    checkbox presque okay

--- blokus-game/src/gui/BlokusCheckBox.java
+++ blokus-game/src/gui/BlokusCheckBox.java
@@ -26,36 +26,23 @@ public class BlokusCheckBox implements DrawableInterface {
 	
 	private BufferedImage noChecked;
 	
-	private BufferedImage checkedHover;
-	
-	private BufferedImage noCheckedHover;
-	
-	private BufferedImage checkedDisable;
-	
-	private BufferedImage noCheckedDisable;
 	
 	private Vector2 position;
-	
-	private boolean mouseHover;
 	
 	private Dimension size;
 	
 	private ArrayList<ActionListener> listeners;
 	
 	public BlokusCheckBox(boolean enabled, boolean checked) {
-		this.mouseHover = false;
+
 		this.isChecked = checked;
 		this.isEnabled = enabled;
 		this.position = new Vector2();
 		this.size = new Dimension(20, 20);
 		this.listeners = new ArrayList<ActionListener>();
 		try {
-			this.checked = ImageIO.read(new File(Page.PATH_RESOURCES_BOUTONS+"box.png"));
-			this.noChecked = ImageIO.read(new File(Page.PATH_RESOURCES_BOUTONS+"boxchecked.png"));
-			this.checkedHover = BufferedImageHelper.generateMask(this.checked, Color.BLACK, 0.5f);
-			this.noCheckedHover = BufferedImageHelper.generateMask(this.noChecked, Color.BLACK, 0.5f);
-			this.checkedDisable = BufferedImageHelper.generateMask(this.checked, Color.BLACK, 0.5f);
-			this.noCheckedDisable = BufferedImageHelper.generateMask(this.noChecked, Color.BLACK, 0.5f);
+			this.checked = ImageIO.read(new File(Page.PATH_RESOURCES_BOUTONS+"boxchecked.png"));
+			this.noChecked = ImageIO.read(new File(Page.PATH_RESOURCES_BOUTONS+"box.png"));
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 			e.printStackTrace();
@@ -106,31 +93,11 @@ public class BlokusCheckBox implements DrawableInterface {
 	@Override
 	public void update(float elapsedTime) {
 		
-		if(this.isInBounds(Mouse.getPosition()) && this.isEnabled)
-		{
-			this.mouseHover = true;
-			if(Mouse.getLastMouseButton() == Mouse.LEFT)
-			{
-				if(this.isChecked) {
-					if(Mouse.isReleased()) {
-						this.raiseClickEvent(new ActionEvent(this, 0, null));
-						this.isChecked = false;
-					}
-				}
-				else {
-					this.isChecked = true;
-				}
+		if(this.isInBounds(Mouse.getPosition()) && this.isEnabled){
+			if(Mouse.getLastMouseButton() == Mouse.LEFT && !Mouse.isReleased()){
+				Mouse.consumeLastMouseButton();
+				this.swapChecked();
 			}
-		}
-		else
-		{
-			this.mouseHover = false;
-			this.isChecked = false;
-		}
-		
-		if(this.isChecked && Mouse.isReleased())
-		{
-			this.isChecked = false;
 		}
 		
 	}
@@ -141,7 +108,7 @@ public class BlokusCheckBox implements DrawableInterface {
 		
 		Graphics2D g2d = (Graphics2D) g.create();
 		g2d.drawImage(this.isChecked ?this.checked: this.noChecked, this.position.getX(), this.position.getY(), (int) this.size.getWidth(), (int) this.size.getHeight(), null);
-		g2d.dispose();
+		
 		
 		
 		/*if(this.backgroundImage != null)
@@ -178,7 +145,7 @@ public class BlokusCheckBox implements DrawableInterface {
 		
 	}
 	
-	public void setChecked(){
+	public void swapChecked(){
 		this.isChecked = !isChecked;
 	}
 	
