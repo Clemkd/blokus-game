20d6f548a9dd9c0921b7826a3ae658e6cb34e6ce
andrev <andrev@127.0.0.1>
2016-05-26 17:57:15 +0200

    fix

--- blokus-game/src/entities/Game.java
+++ blokus-game/src/entities/Game.java
@@ -19,6 +19,7 @@ import java.util.List;
 import javax.swing.JDialog;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
