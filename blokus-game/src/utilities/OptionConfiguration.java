c89158160dbf23d48e234247f9224ba09e79f11c
andrev <andrev@127.0.0.1>
2016-05-30 17:35:01 +0200

    début des options Configuration

--- blokus-game/src/utilities/OptionConfiguration.java
+++ blokus-game/src/utilities/OptionConfiguration.java
@@ -1,6 +1,38 @@
 package utilities;
 
-public class OptionConfiguration {
+import java.io.Serializable;
+
+public class OptionConfiguration implements Serializable {
+	
+	private boolean playSong;
+	
+	public boolean isPlaySong() {
+		return playSong;
+	}
+
+	public boolean isHelp() {
+		return help;
+	}
+
+	public float getVolume() {
+		return volume;
+	}
+
+	public boolean isAutoSave() {
+		return autoSave;
+	}
+
+	public boolean isDaltonienMode() {
+		return daltonienMode;
+	}
+
+	private boolean help;
+	
+	private float volume;
+	
+	private boolean autoSave;
+	
+	private boolean daltonienMode;
 	
 	public OptionConfiguration() {
 		
