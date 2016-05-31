5a8a501e4735dce7a73ced9d569145a149d1c138
andrev <andrev@127.0.0.1>
2016-05-31 13:27:38 +0200

    chargement des options au lancement

--- blokus-game/src/program/Program.java
+++ blokus-game/src/program/Program.java
@@ -2,9 +2,12 @@ package program;
 import javax.swing.SwingUtilities;
 
 import gui.Window;
+import utilities.OptionConfiguration;
 
 public class Program 
 {
+	OptionConfiguration optionConfiguration = OptionConfiguration.loadOption();
+	
 	public static void main(String[] args) {
 		Window window = new Window("Blokus");
 		SwingUtilities.invokeLater(window);
