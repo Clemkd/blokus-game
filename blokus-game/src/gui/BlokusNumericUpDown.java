993f19ede69b154fb7043ed4d8a4bf945ca714f4
andrev <andrev@127.0.0.1>
2016-06-01 11:11:33 +0200

    class numeric up down

---
+++ blokus-game/src/gui/BlokusNumericUpDown.java
@@ -0,0 +1,122 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import navigation.Page;
+import program.Program;
+import utilities.Vector2;
+
+public class BlokusNumericUpDown implements DrawableInterface{
+	
+	private String text;
+	
+	private BlokusButton up;
+	
+	private BlokusButton down;
+	
+	private Vector2 position;
+	
+	private Vector2 positionText;
+	
+	private Dimension size;
+	
+	private ArrayList<ActionListener> listeners;
+	
+	private Font font;
+	
+	private Rectangle2D textSize;
+	
+	public BlokusNumericUpDown(String text, Font font) {
+		this.text = text;
+		this.font = font;
+		this.position = new Vector2();
+		this.positionText = new Vector2();
+		this.size = new Dimension();
+		this.up = new BlokusButton(getClass().getResource(Page.PATH_RESOURCES_BOUTONS+"boxvolumeplus.png"));
+		this.down = new BlokusButton(getClass().getResource(Page.PATH_RESOURCES_BOUTONS+"boxvolumemoins.png"));
+		
+		AffineTransform affinetransform = new AffineTransform();     
+		FontRenderContext frc = new FontRenderContext(affinetransform,true,true);
+		this.textSize = this.font.getStringBounds(this.text, frc);
+	}
+	
+	
+	@Override
+	public void update(float elapsedTime) {
+		this.up.update(elapsedTime);
+		this.down.update(elapsedTime);
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		this.up.draw(g2d);
+		this.down.draw(g2d);
+		g2d.setFont(this.font);
+		g2d.setColor(Color.WHITE);
+		g2d.drawString(this.text, this.positionText.getX(), this.position.getY());
+		System.out.println(this.positionText);
+		g2d.dispose();
+	}
+	
+	public Vector2 getPosition() {
+		return position;
+	}
+	
+	public void setPosition(Vector2 position) {
+		this.down.setPosition(position);
+		this.positionText.setX((int) (this.down.getSize().getWidth()+this.down.getPosition().getX()));
+		this.positionText.setY(position.getY());
+		System.out.println(this.positionText);
+		this.up.setPosition(new Vector2((int) (this.positionText.getX()+this.textSize.getWidth()), position.getY()));
+	}
+	
+	public Dimension getSize() {
+		return size;
+	}
+	
+	public void setSize(Dimension size) {
+		this.size = size;
+	}
+	
+	public boolean isInBoundsUp(Vector2 p)
+	{
+		return 	p.getX() >= this.position.getX()+this.down.getSize().getWidth()+this.textSize.getWidth() &&
+				p.getX() < this.up.getSize().getWidth() + this.position.getX() + this.down.getSize().getWidth()+this.textSize.getWidth() &&
+				p.getY() >= this.position.getY() &&
+				p.getY() < this.getSize().getHeight() + this.position.getY();
+	}
+	
+	public boolean isInBoundsDown(Vector2 p)
+	{
+		return 	p.getX() >= this.position.getX() &&
+				p.getX() < this.getSize().getWidth() + this.position.getX() &&
+				p.getY() >= this.position.getY() &&
+				p.getY() < this.getSize().getHeight() + this.position.getY();
+	}
+	
+	public void addListener(ActionListener listener){
+		this.listeners.add(listener);
+	}
+	
+	public void raiseClickEvent(ActionEvent e)
+	{
+		for(ActionListener listener : this.listeners)
+		{
+			listener.actionPerformed(e);
+		}
+	}
+}
