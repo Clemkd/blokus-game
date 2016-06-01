16aeadffdab72ec05ac95c1e16f5520221b22a6e
hoangtha <momojean0@gmail.com>
2016-06-01 15:00:59 +0200

    Tu cherchais juste un jeu sur le chemin de la classe..

--- blokus-game/src/entities/Game.java
+++ blokus-game/src/entities/Game.java
@@ -1,8 +1,6 @@
 package entities;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
